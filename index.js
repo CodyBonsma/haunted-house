b09cd05337ba503a858c4940ddf6a82bb1efefd2
unknown <codybonsma@gmail.com>
2022-01-03 16:19:08 -0500

    added sessions

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
 const express = require('express');
+const session = require('express-session');
 const app = express();
 const port = process.env.PORT || '8080';
 
 app.use(express.static(__dirname + '/public'));
 app.use('/public', express.static('public'))
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(session({
+    secret: 'qrkfnqerf',
+    resave: false,
+    saveUninitialized: true,
+    cookie: {secure: false}
+}));
+
+
 
 const text1 = false;
 const text2 = false;
 
 
 app.get('/', (req,res) => {
+    const session_username = req.session ? req.session.username : "no user set";
+    res.render('homepage', {my_user: session_username});
+});
+
+app.get('/homepage', (req,res) => {
     res.render('home');
 });
+
+app.get('/home', (req,res) => {
+    if (req.session && req.session.username){
+        const session_username = req.session ? req.session.username : "no user set";
+        res.render('home', {my_user: session_username});
+    } else {
+        res.redirect('/');
+    }
+    
+});
+
+
+app.post('/signup', (req,res)=>{
+    const valid_users = [
+        {'name': 'sue', 'password': 'sue'},
+        {'name': 'joe', 'password': 'joe'},
+        {'name': 'silvan', 'password': 'silvan'},
+        {'name': 'lee', 'password': 'lee'},
+    ];
+
+    const user = req.body.username;
+    const pass = req.body.password;
+    
+    const found_user = valid_users.find(usr => usr.name == user && usr.password == pass);
+
+    if (found_user){
+        req.session.username = user;
+        res.redirect('/home')
+    } else {
+        req.session.destroy(() => {
+            console.log('user reset')
+        });
+        res.redirect('/');
+    }
+
+  });
 
 app.get('/entry', (req,res) => {
     res.render('entry', {data: false});
